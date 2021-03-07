d1b8b6ad023e06cc07159beaf62a3a727256febe
alu0101133355 <alu0101133355@ull.edu.es>
2021-03-07 13:49:01 +0000

    Ejercicio 3

---
+++ ejercicios/dist/ejercicio3.d.ts
@@ -0,0 +1,5 @@
+/**
+ Validar mensajes, desarrollar una funcion denominada isValid que reciba como parametro
+ una cadena compuesta por numeros y letras y determine si es valida. True si lo es
+ */
+declare function isValid(cadena: string): boolean;
