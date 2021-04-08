0ce352e024de3732774de5597fa137ba1b662faf
alu0101133355 <72798354+alu0101133355@users.noreply.github.com>
2021-04-08 11:32:49 +0100

    Add files via upload

---
+++ practica4/src/pe_103_LIFO.ts
@@ -0,0 +1,54 @@
+/**
+ * Interface tipo Generica para implementar una
+ * pila tipo LIFO
+ * PE 103 
+ * AUTOR: Javier Martin de Leon
+ */
+
+interface LIFOInterface<T> {
+
+    push(element: T): void;
+
+    pop(): void;
+
+    pseek(): T;
+
+    size(): number;
+
+    print(): void;
+
+}
+
+export class Lifo<T> implements LIFOInterface<T> {
+
+  constructor(private array_lifo: T[]) {}
+
+
+  getarray_lifo(): T[] {
+    return this.array_lifo;
+  }
+
+/* Añade un element a la pila
+*/
+  push(element: T): void {
+    this.array_lifo.push(element);
+  }
+
+/* Extrae el ultimo element insertado en la pila 
+*/
+
+  pop():void {
+    this.array_lifo.splice(this.array_lifo.length - 1, 1);
+  }
+
+/* Visualiza el contenido de la pila
+*/
+  print(): void {
+    console.log('Mostrando nuestra pila duracell');
+    for (let index = this.array_lifo.length - 1; index >= 0; index--) {
+
+      console.log(this.array_lifo[index]);
+
+    }
+  }
+}
